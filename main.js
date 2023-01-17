a3c1aa9d64829cd57ccc99d8e9d38f3acfbdc886
adryan2022 <117435631+adryan2022@users.noreply.github.com>
2023-01-17 10:26:32 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-var logo = document.querySelector('.containerburger');
-var menu = document.querySelector('.menuburger');
+const logo = document.querySelector('.containerburger');
+const menu = document.querySelector('.menuburger-wrapper');
+const closeBtn = document.querySelector(".close-button");
+
+
 
 
 logo.addEventListener('click', function(){
-    menu.classList.toggle('showmenuburger');
-} )
+  
+    menu.classList.add('showmenuburger');
+} );
+
+
+closeBtn.addEventListener("click", ()=>{
+    menu.classList.remove('showmenuburger');
+});
